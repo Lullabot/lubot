e380e8652ae7ae8f4a37eaa9db5f101f3e74496e
Sally Young <sally@justafish.co.uk>
2014-02-28 15:33:47 -0800

    Fix github sender

--- scripts/github.js
+++ scripts/github.js
@@ -13,11 +13,11 @@ module.exports = function(bot, app) {
           var channel = githubConfig[repo];
           // Issues
           if (typeof payload.issue !== 'undefined') {
-            bot.irc.say(channel, '[ ' + payload.issue.user.login + ' ' + payload.action + ' issue #' + payload.issue.number + ': ' + payload.issue.title + ' ' + payload.issue.html_url + ' ]')
+            bot.irc.say(channel, '[ ' + payload.sender.login + ' ' + payload.action + ' issue #' + payload.issue.number + ': ' + payload.issue.title + ' ' + payload.issue.html_url + ' ]')
           }
           // Pull Requests
           else if (typeof payload.pull_request !== 'undefined') {
-            bot.irc.say(channel, '[ ' + payload.pull_request.user.login + ' ' + payload.action + ' pull request #' + payload.pull_request.number + ': ' + payload.pull_request.title + ' ' + payload.pull_request.html_url + ' ]')
+            bot.irc.say(channel, '[ ' + payload.sender.login + ' ' + payload.action + ' pull request #' + payload.pull_request.number + ': ' + payload.pull_request.title + ' ' + payload.pull_request.html_url + ' ]')
           }
         }
       }
