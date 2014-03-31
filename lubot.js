d5c0de342a6bf11c2750f48257f2d4f099ddef1e
Jerad Bitner <sirkitree@gmail.com>
2014-03-31 17:53:01 -0600

    Initial cleanups recommended by jslint

--- lubot.js
+++ lubot.js
@@ -9,18 +9,17 @@ var config = {
   mongoPrefix: process.env.LUBOT_MONGOPREFIX
 };
 
-if (typeof process.env.LUBOT_IRC_NICK_PW !== 'undefined') {
-  config.botPassword = process.env.LUBOT_IRC_NICK_PW;
-}
-else {
+if (process.env.LUBOT_IRC_NICK_PW === 'undefined') {
   config.botPassword = null;
+} else {
+  config.botPassword = process.env.LUBOT_IRC_NICK_PW;
 }
 
 var express = require('express');
 var app = express();
 app.use(express.json());
 app.use(express.urlencoded());
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.send('Hello, world!');
 });
 app.listen(config.webPort);
@@ -38,7 +37,7 @@ bot.irc = new irc.Client(config.server, config.botName, {
   secure: true,
   stripColors: true
 });
-bot.irc.once("registered", function(channel, who) {
+bot.irc.once("registered", function () {
   console.log('Connected to ' + config.server);
   bot.irc.say('NickServ', 'IDENTIFY ' + config.botName + ' ' + config.botPassword);
 });
@@ -47,8 +46,8 @@ bot.irc.once("registered", function(channel, who) {
 bot.irc.setMaxListeners(50);
 
 // Catch IRC errors.
-bot.irc.addListener('error', function(message) {
-    console.log('error: ', message);
+bot.irc.addListener('error', function (message) {
+  console.log('error: ', message);
 });
 
 /**
@@ -66,10 +65,10 @@ bot.helpers.utils = {
    * @return string|bool
    *   The string with the token cut out, or false if it wasn't found.
    */
-  startsWith: function(token, text) {
+  startsWith: function (token, text) {
     var start = text.slice(0, token.length);
     if (start === token) {
-      newText = text.slice(token.length);
+      var newText = text.slice(token.length);
       if (newText.length > 0) {
         return newText;
       }
@@ -85,11 +84,11 @@ bot.helpers.utils = {
    * @return string|bool
    *   The string with the token cut out, or false if it wasn't found.
    */
-  startsBot: function(text) {
+  startsBot: function (text) {
     var re = new RegExp(bot.irc.opt.nick + "(.*?) (.*)", "gi");
     var matches = re.exec(text);
     if (!bot.helpers.utils.empty(matches, 1) && !bot.helpers.utils.empty(matches, 2)) {
-      return matches[2]
+      return matches[2];
     }
     return false;
   },
@@ -105,12 +104,11 @@ bot.helpers.utils = {
    * @return string|bool
    *   The string with the token cut out, or false if it wasn't found.
    */
-  startsWithBot: function(token, text) {
-    botText = this.startsBot(text);
+  startsWithBot: function (token, text) {
+    var botText = this.startsBot(text);
     if (botText !== false) {
       text = this.startsWith(token, botText);
-    }
-    else {
+    } else {
       text = this.startsWith(token, text);
     }
     return text;
@@ -126,10 +124,10 @@ bot.helpers.utils = {
    * @return string|bool
    *   The string with the token cut out, or fale if it wasn't found
    */
-  endsWith: function(token, text) {
+  endsWith: function (token, text) {
     var end = text.slice(0 - token.length);
     if (end === token) {
-      newText = text.slice(0, text.length - token.length);
+      var newText = text.slice(0, text.length - token.length);
       if (newText.length > 0) {
         return newText;
       }
@@ -145,11 +143,13 @@ bot.helpers.utils = {
    * @return Array
    *   The processed array.
    */
-  unique: function(array) {
-    var a = array.concat();
-    for(var i=0; i<a.length; ++i) {
-      for(var j=i+1; j<a.length; ++j) {
-        if(a[i] === a[j]) {
+  unique: function (array) {
+    var a = array.concat(),
+      i = 0,
+      j = 0;
+    for (i = 0; i < a.length; ++i) {
+      for (j = i + 1; j < a.length; ++j) {
+        if (a[i] === a[j]) {
           a.splice(j--, 1);
         }
       }
@@ -164,7 +164,7 @@ bot.helpers.utils = {
    *
    * @return string
    */
-  timeSince: function(timestamp) {
+  timeSince: function (timestamp) {
     var seconds = Math.floor((new Date() - timestamp) / 1000);
     var interval = Math.floor(seconds / 31536000);
 
@@ -197,7 +197,7 @@ bot.helpers.utils = {
    *
    * @return bool
    */
-  empty: function(object, key) {
+  empty: function (object, key) {
     if (object !== null && object.hasOwnProperty(key)) {
       return false;
     }
@@ -210,10 +210,9 @@ bot.helpers.utils = {
  * to specific collections.
  */
 bot.brain = {
-  mongoClient: function() {
-    var MongoClient = require('mongodb').MongoClient
-        , format = require('util').format;
-        return MongoClient;
+  mongoClient: function () {
+    var MongoClient = require('mongodb').MongoClient;
+    return MongoClient;
   },
   /**
    * Save an item into the Key/Value store.
@@ -223,9 +222,9 @@ bot.brain = {
    * @param (optional) collection_name
    *   Store items into their own collection
    */
-  saveKV: function(key, value, collection_name) {
-    this.mongoClient().connect(config.mongoUrl, function(err, db) {
-      if (err) throw err;
+  saveKV: function (key, value, collection_name) {
+    this.mongoClient().connect(config.mongoUrl, function (err, db) {
+      if (err) { throw err; }
       var collection_n = config.mongoPrefix + 'kv';
       if (collection_name !== null && typeof collection_name === 'string') {
         collection_n = config.mongoPrefix + collection_name;
@@ -233,10 +232,11 @@ bot.brain = {
       var collection = db.collection(collection_n);
       collection.findAndModify(
         {key: key.toLowerCase()},
-        [['_id','asc']],
+        [['_id', 'asc']],
         {$set: {value: value}},
         {upsert: true},
-        function(err, object) {
+        function (err) {
+          if (err) { throw err; }
           db.close();
         }
       );
@@ -251,15 +251,16 @@ bot.brain = {
    * @param (optional) function success
    *   @return function(value){}
    */
-  loadKV: function(key, collection_name, success) {
-    this.mongoClient().connect(config.mongoUrl, function(err, db) {
-      if (err) throw err;
+  loadKV: function (key, collection_name, success) {
+    this.mongoClient().connect(config.mongoUrl, function (err, db) {
+      if (err) { throw err; }
       var collection_n = config.mongoPrefix + 'kv';
       if (collection_name !== null && typeof collection_name === 'string') {
         collection_n = config.mongoPrefix + collection_name;
       }
       var collection = db.collection(collection_n);
-      collection.findOne({key: key.toLowerCase()}, function(err, document) {
+      collection.findOne({key: key.toLowerCase()}, function (err, document) {
+        if (err) { throw err; }
         if (document && document.value && typeof success === 'function') {
           success(document.value);
         }
@@ -274,16 +275,17 @@ bot.brain = {
    * @param (optional) collection_name
    *   If this item has been stored into it's own collection, provide the name here.
    */
-  removeKV: function(key, collection_name) {
+  removeKV: function (key, collection_name) {
     if (typeof key === 'string') {
-      this.mongoClient().connect(config.mongoUrl, function(err, db) {
-        if (err) throw err;
+      this.mongoClient().connect(config.mongoUrl, function (err, db) {
+        if (err) { throw err; }
         var collection_n = config.mongoPrefix + 'kv';
         if (collection_name !== null && typeof collection_name === 'string') {
           collection_n = config.mongoPrefix + collection_name;
         }
         var collection = db.collection(collection_n);
-        collection.remove({key: key}, {w:1}, function(err, numberOfRemovedDocs) {
+        collection.remove({key: key}, {w: 1}, function (err) {
+          if (err) { throw err; }
           db.close();
         });
       });
@@ -295,13 +297,14 @@ bot.brain = {
    *
    * @param object data
    */
-  messageLog: function(data) {
+  messageLog: function (data) {
     if (data !== null && typeof data === 'object') {
-      this.mongoClient().connect(config.mongoUrl, function(err, db) {
-        if (err) throw err;
+      this.mongoClient().connect(config.mongoUrl, function (err, db) {
+        if (err) { throw err; }
         data.time = new Date().getTime();
         var collection  = db.collection(config.mongoPrefix + 'logs');
-        collection.insert(data, {safe: true}, function(err, records) {
+        collection.insert(data, {safe: true}, function (err) {
+          if (err) { throw err; }
           db.close();
         });
       });
@@ -316,18 +319,16 @@ bot.brain = {
    * @param function success
    *   @return function(docs) {}
    */
-  loadMessageLog: function(search, options, success) {
+  loadMessageLog: function (search, options, success) {
     if (typeof search === 'object' && typeof options === 'object' && typeof success === 'function') {
-      this.mongoClient().connect(config.mongoUrl, function(err, db) {
-        if (err) throw err;
-        options_m = {};
-        if (options !== null && typeof options === "object") {
-          options_m = options;
-        }
+      this.mongoClient().connect(config.mongoUrl, function (err, db) {
+        if (err) { throw err; }
         var collection = db.collection(config.mongoPrefix + 'logs');
-        collection.find(search, options, function(err, cursor) {
+        collection.find(search, options, function (err, cursor) {
+          if (err) { throw err; }
           if (cursor !== null) {
-            cursor.toArray(function(err, docs) {
+            cursor.toArray(function (err, docs) {
+              if (err) { throw err; }
               success(docs);
               db.close();
             });
@@ -344,9 +345,11 @@ bot.brain = {
    *
    * @return ?
    */
-  searchMessageLog: function(text) {
-    this.mongoClient().connect(config.mongoUrl, function(err, db) {
-      db.command({text: config.mongoPrefix + 'logs', search: text}, function(err, objects) {
+  searchMessageLog: function (text) {
+    this.mongoClient().connect(config.mongoUrl, function (err, db) {
+      if (err) { throw err; }
+      db.command({text: config.mongoPrefix + 'logs', search: text}, function (err, objects) {
+        if (err) { throw err; }
         console.log(objects);
         db.close();
       });
@@ -358,11 +361,12 @@ bot.brain = {
    * @param string collection_name
    * @param object data
    */
-  saveToCollection: function(collection_name, data) {
-    this.mongoClient().connect(config.mongoUrl, function(err, db) {
-      if (err) throw err;
+  saveToCollection: function (collection_name, data) {
+    this.mongoClient().connect(config.mongoUrl, function (err, db) {
+      if (err) { throw err; }
       var collection = db.collection(config.mongoPrefix + collection_name);
-      collection.insert(data, {safe: true}, function(err, records) {
+      collection.insert(data, {safe: true}, function (err) {
+        if (err) { throw err; }
         db.close();
       });
     });
@@ -374,14 +378,16 @@ bot.brain = {
    * @param object search
    * @param object data
    */
-  upsertToCollection: function(collection_name, search, data) {
-    this.mongoClient().connect(config.mongoUrl, function(err, db) {
-      if (err) throw err;
+  upsertToCollection: function (collection_name, search, data) {
+    this.mongoClient().connect(config.mongoUrl, function (err, db) {
+      if (err) { throw err; }
       var collection = db.collection(config.mongoPrefix + collection_name);
       collection.update(
         search,
         {$set: data},
-        {upsert: true}, function(err, count) {
+        {upsert: true},
+        function (err) {
+          if (err) { throw err; }
           db.close();
         }
       );
@@ -398,18 +404,16 @@ bot.brain = {
    * @param function success
    *   @return function(docs) {}
    */
-  loadFromCollection: function(collection_name, search, options, success) {
+  loadFromCollection: function (collection_name, search, options, success) {
     if (typeof collection_name === 'string' && typeof search === 'object' && typeof options === 'object' && typeof success === 'function') {
-      this.mongoClient().connect(config.mongoUrl, function(err, db) {
-        if (err) throw err;
-        options_m = {};
-        if (options !== null && typeof options === "object") {
-          options_m = options;
-        }
+      this.mongoClient().connect(config.mongoUrl, function (err, db) {
+        if (err) { throw err; }
         var collection = db.collection(config.mongoPrefix + collection_name);
-        collection.find(search, options, function(err, cursor) {
+        collection.find(search, options, function (err, cursor) {
+          if (err) { throw err; }
           if (cursor !== null) {
-            cursor.toArray(function(err, docs) {
+            cursor.toArray(function (err, docs) {
+              if (err) { throw err; }
               success(docs);
               db.close();
             });
@@ -425,12 +429,13 @@ bot.brain = {
    * @param object search parameters
    *   {nick: "lubot"}
    */
-  removeFromCollection: function(collection_name, search) {
+  removeFromCollection: function (collection_name, search) {
     if (typeof collection_name === 'string' && typeof search === 'object') {
-      this.mongoClient().connect(config.mongoUrl, function(err, db) {
-        if (err) throw err;
+      this.mongoClient().connect(config.mongoUrl, function (err, db) {
+        if (err) { throw err; }
         var collection = db.collection(config.mongoPrefix + collection_name);
-        collection.remove(search, {w:1}, function(err, numberOfRemovedDocs) {
+        collection.remove(search, {w: 1}, function (err) {
+          if (err) { throw err; }
           db.close();
         });
       });
@@ -445,10 +450,10 @@ bot.brain = {
    * @param (optional) string collection_name
    *   If this item has been stored into it's own collection, provide the name here.
    */
-  incValue: function(key, amount, collection_name) {
+  incValue: function (key, amount, collection_name) {
     if (key !== null && amount !== null && typeof amount === 'number') {
-      this.mongoClient().connect(config.mongoUrl, function(err, db) {
-        if (err) throw err;
+      this.mongoClient().connect(config.mongoUrl, function (err, db) {
+        if (err) { throw err; }
         var collection_n = config.mongoPrefix + 'kv';
         if (collection_name !== null && typeof collection_name === "string" && collection_name.length > 0) {
           collection_n = config.mongoPrefix + collection_name;
@@ -457,15 +462,15 @@ bot.brain = {
         var search = {};
         if (typeof key === 'string') {
           search.key = key;
-        }
-        else if (typeof key === 'object') {
+        } else if (typeof key === 'object') {
           search = key;
         }
         collection.update(
           search,
           {$inc: {value: amount}},
           {upsert: true},
-          function(err, object) {
+          function (err) {
+            if (err) { throw err; }
             db.close();
           }
         );
@@ -486,26 +491,30 @@ bot.helpers.irc = {
    *
    * @return object
    */
-  'channelUsers': function(channel) {
-    if (typeof channel !== "undefined" && typeof bot.irc.chans !== "undefined" && typeof bot.irc.chans[channel] !== "undefined" && typeof bot.irc.chans[channel].users !== "undefined") {
-      return bot.irc.chans[channel].users;
+  'channelUsers': function (channel) {
+    if (channel === "undefined"
+        && bot.irc.chans === "undefined"
+        && bot.irc.chans[channel] === "undefined"
+        && bot.irc.chans[channel].users === "undefined") {
+      return {};
     }
-    return {};
+    return bot.irc.chans[channel].users;
   },
 };
 
 /**
  * Logger
  */
-bot.brain.mongoClient().connect(config.mongoUrl, function(err, db) {
-  db.ensureIndex(config.mongoPrefix + 'logs', {text: "text"}, function(err, obj) {});
-  bot.irc.addListener("message#", function(nick, to, text, message) {
+bot.brain.mongoClient().connect(config.mongoUrl, function (err, db) {
+  if (err) { throw err; }
+  db.ensureIndex(config.mongoPrefix + 'logs', {text: "text"});
+  bot.irc.addListener("message#", function (nick, to, text) {
     bot.brain.messageLog({nick: nick, channel: to, text: text});
   });
 });
 
 // Load Scripts
-require("fs").readdirSync("./scripts").forEach(function(file) {
+require("fs").readdirSync("./scripts").forEach(function (file) {
   if (bot.helpers.utils.endsWith(".js", file) !== false) {
     require("./scripts/" + file)(bot, app);
   }
