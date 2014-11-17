bb43e681363cd0a0c9cd6e253054236fae676276
Joe Shindelar <eojthebrave@gmail.com>
2014-11-16 21:38:18 -0700

    [#7] Allow factoids to be word case agnositc.

--- scripts/factoids.js
+++ scripts/factoids.js
@@ -26,7 +26,7 @@ module.exports = function(bot) {
       var re = /(.+?)\sis\s(.+)/;
       var matches = re.exec(text);
       if (!bot.helpers.utils.empty(matches, 1) && !bot.helpers.utils.empty(matches, 2)) {
-        bot.brain.upsertToCollection('factoids', {key: matches[1], channel: to}, {key: matches[1], channel: to, factoid: matches[2]});
+        bot.brain.upsertToCollection('factoids', {key: matches[1].toLowerCase(), channel: to}, {key: matches[1].toLowerCase(), channel: to, factoid: matches[2].toLowerCase()});
         bot.irc.say(to, nick + ': Okay!');
       }
     }
@@ -34,7 +34,7 @@ module.exports = function(bot) {
     // Delete a factoid.
     var delText = bot.helpers.utils.startsWith('delete factoid ', text);
     if (delText !== false) {
-      bot.brain.removeFromCollection('factoids', {key: delText, channel: to});
+      bot.brain.removeFromCollection('factoids', {key: delText.toLowerCase(), channel: to});
       bot.irc.say(to, nick + ': Okay!');
     }
 
@@ -48,7 +48,7 @@ module.exports = function(bot) {
       factoid = bot.helpers.utils.endsWith('!', factoid);
     }
     if (factoid !== false) {
-      bot.brain.loadFromCollection('factoids', {key: factoid, channel: to}, {}, function(docs) {
+      bot.brain.loadFromCollection('factoids', {key: factoid.toLowerCase(), channel: to}, {}, function(docs) {
         if (docs.hasOwnProperty(0)) {
           bot.irc.say(to, factoid + ' is ' + docs[0].factoid);
         }
