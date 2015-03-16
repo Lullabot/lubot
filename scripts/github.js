b048f0fa937f1db2f2508b7198d81daeff717186
William Hetherington <me@willwh.com>
2015-03-16 09:35:31 -0700

    Replace username so we don't hilight people every time they do something in github

--- scripts/github.js
+++ scripts/github.js
@@ -9,18 +9,24 @@ module.exports = function(bot, app) {
       var payload = request.body;
       if (payload.action !== null) {
         var repo = payload.repository.full_name;
+        var ghuser = replaceAt(payload.sender.login, 1, "_")
         if (typeof githubConfig[repo] === 'string') {
           var channel = githubConfig[repo];
           // Issues
           if (typeof payload.issue !== 'undefined') {
-            bot.irc.say(channel, '[ ' + payload.sender.login + ' ' + payload.action + ' issue #' + payload.issue.number + ': ' + payload.issue.title + ' ' + payload.issue.html_url + ' ]')
+            bot.irc.say(channel, '[ ' + ghuser + ' ' + payload.action + ' issue #' + payload.issue.number + ': ' + payload.issue.title + ' ' + payload.issue.html_url + ' ]')
           }
           // Pull Requests
           else if (typeof payload.pull_request !== 'undefined') {
-            bot.irc.say(channel, '[ ' + payload.sender.login + ' ' + payload.action + ' pull request #' + payload.pull_request.number + ': ' + payload.pull_request.title + ' ' + payload.pull_request.html_url + ' ]')
+            bot.irc.say(channel, '[ ' + ghuser + ' ' + payload.action + ' pull request #' + payload.pull_request.number + ': ' + payload.pull_request.title + ' ' + payload.pull_request.html_url + ' ]')
           }
         }
       }
     }
   });
 };
+
+function replaceAt(s, n, t) {
+    return s.substring(0, n) + t + s.substring(n + 1);
+}
+
