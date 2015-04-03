52eefe1c1e89f7a28c06b3ba35dd734f8523d3e1
William Hetherington <me@willwh.com>
2015-04-02 22:25:17 -0700

    highfive, yo

---
+++ scripts/highfive.js
@@ -0,0 +1,30 @@
+/**
+ * Description:
+ *   Broadcasts a message to all users of a channel.
+ *
+ * Commands:
+ *   lubot all: <text>
+ *
+ **/
+
+module.exports = function(bot) {
+
+  // Respond to build results.
+  bot.irc.addListener("message#", function(nick, to, text, message) {
+    console.log("nick: " + nick);
+    // Remove bot name.
+    botText = bot.helpers.utils.startsBot(text);
+    if (botText !== false) {
+      text = botText;
+    }
+
+    if(nick == 'lullajenky') {
+      if (text.indexOf("SUCCESS") > -1) {
+        bot.irc.action(to, 'highfives lullajenky');
+      }
+      else if (text.indexOf("FAIL") > -1) {
+        bot.irc.action(to, nick + "weeps uncontrollably");
+      }
+    }
+  });
+};
